57867e5fbf69f375cfe0ca66f0d9f886e36ced48
skylerknecht <skyler.knecht@outlook.com>
2024-05-18 20:40:01 -0400

    Added Kevin to the banner

--- messenger/__init__.py
+++ messenger/__init__.py
@@ -6,5 +6,5 @@ banner = f"""
 | |\/| |/ _ \/ __/ __|/ _ \ '_ \ / _` |/ _ \ '__|
 | |  | |  __/\__ \__ \  __/ | | | (_| |  __/ |   
 |_|  |_|\___||___/___/\___|_| |_|\__, |\___|_|   
-   by Skyler Knecht              |___/ v{__version__}
+by Skyler Knecht and Kevin Clark |___/ v{__version__}
 """
