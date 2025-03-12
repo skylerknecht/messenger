e9075d24f41447e30522de9f4ad74e91f2bed57e
skylerknecht <skyler.knecht@outlook.com>
2025-03-11 23:25:34 -0400

    removed the double space after the arrow for currently interacted messengers

--- messenger/manager.py
+++ messenger/manager.py
@@ -416,7 +416,7 @@ class Manager:
                 )
                 for forwarder in messenger.forwarders
             ]
-            current_messenger_identifier = f"{self.update_cli.color_text('>', 'red')}  {self.update_cli.bold_text(messenger.identifier)}"
+            current_messenger_identifier = f"{self.update_cli.color_text('>', 'red')} {self.update_cli.bold_text(messenger.identifier)}"
             messenger_identifier = self.update_cli.bold_text(messenger.identifier)
             identifier = current_messenger_identifier if self.current_messenger == messenger else messenger_identifier
             item = {
